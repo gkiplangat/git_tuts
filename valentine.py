ae4de21c6a215e9d974fa913353754d890d46251
gkiplangat <gkiplangat>
2023-02-05 23:16:38 +0300

    deleted

--- valentine.py
+++ valentine.py
@@ -1,4 +1,4 @@
-#Ask for the user age and store in a variable
-#print out the age to the user
-num = input('Enter your age: ')
-print('You are: ' + num + 'years old')
+#!/usr/bin/python
+
+# Hello world python program
+
