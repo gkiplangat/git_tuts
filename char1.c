d283456a8ebd6cb0ad2d2d83fede2d8dc01d3e72
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-28 03:41:39 -0800

    size of the char

---
+++ char1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char c = 'H';
+	/*Print out character */
+	printf("%c\n", c);
+	/*Print out the ascii related to the character*/
+	printf("%d\n", c);
+	return (0);
+}
+
