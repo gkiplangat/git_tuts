83cccf3ef8e374491196e0e263d218cf4cb5a65f
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-28 04:37:27 -0800

    if statements in pointers and dereferencing

---
+++ pointer3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+	int n = 98;
+	int *p = &n;
+	printf("%d\n", n); /*the value of variable n*/
+	printf("%p\n", p); /*the address of variable n*/
+	printf("%p\n", &p); /* the address of pointer p */
+
+	/*if statements in pointers */
+	if (n < 100)
+	{
+		*p = 402;
+		printf("%d\n", n);
+	}
+	return (0);
+}
+
