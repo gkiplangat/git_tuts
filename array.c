d8caa5b8cc16c47b835da39365e8770f30511014
gkiplangat <gideonkiplangat4@gmail.com>
2023-03-01 05:52:28 -0800

    arrays

---
+++ array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int nums[5];
+
+	nums[0] = 10;
+	nums[1] = 20;
+	nums[2] = 30;
+	nums[3] = 40;
+	nums[4] = 50;
+
+	printf("The value of nums[0]:%d\n", nums[0]);
+}
